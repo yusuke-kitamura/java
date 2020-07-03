6161679d911c7eba3e9e4d4e729e29a153342434
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-07-03 10:43:04 +0900

    [add]break文ファイルを追加

---
+++ JavaWcp/src/Break01.java
@@ -0,0 +1,12 @@
+
+public class Break01 {
+	public static void main(String[] args) {
+		for (int count = 0; count < 5; count++ ) {
+			if (count == 5) {
+				break;
+			}
+			System.out.println("break01=" + count);
+		}
+	}
+
+}
