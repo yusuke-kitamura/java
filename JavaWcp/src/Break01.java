6f39855f97f92cee5b3f49e3d99267154c084574
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-07-03 10:58:06 +0900

    [add]再起処理文ファイルを追加

--- JavaWcp/src/Break01.java
+++ JavaWcp/src/Break01.java
@@ -7,6 +7,13 @@ public class Break01 {
 			}
 			System.out.println("break01=" + count);
 		}
-	}
 
+		for (int count = 0; count < 5; count++) {
+            if (count == 2) {
+                // countが2の場合、この後の処理をスキップ
+                continue;
+            }
+            System.out.println("Continue01 = " + count);
+        }
+	}
 }
