ce3caab3a297be9b984d06609ab40992517e4ada
yusuke-kitamura <yusuke1209kitamura@gmail.com>
2020-07-19 00:03:22 +0900

    [update]クラスファイルを追加

--- JavaWcp/src/classMethod/ClassMethod01.java
+++ JavaWcp/src/classMethod/ClassMethod01.java
@@ -1,5 +1,18 @@
 package classMethod;
 
+import classMethod.human.Human01;
+
 public class ClassMethod01 {
+	public static void main(String[] args) {
+		// Human01クラスを引数なしコンストラクタでインスタンスを生成
+        Human01 yamada = new Human01();
+        // 初期値が設定されたフィールドを参照して表示
+        System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です。");
+
+        // Human01クラスを引数ありコンストラクタでインスタンスを生成
+        Human01 sato = new Human01("佐藤", 25);
+        // 引数の値が設定されたフィールドを参照して表示
+        System.out.println("名前は" + sato.name + "で、年齢は" + sato.age + "です。");
+	}
 
 }
